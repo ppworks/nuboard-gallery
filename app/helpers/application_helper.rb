2e262cbf2faf737e94ce2e5dda1dc9030c852dbc
koshikawa <koshikawa@ppworks.jp>
2014-03-19 09:01:56 +0900

    Benri helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def human(model_class, attr_name)
+    model_class.human_attribute_name(attr_name)
+  end
+
   def boolean_icon(bool)
     return unless bool
     capture_haml do
